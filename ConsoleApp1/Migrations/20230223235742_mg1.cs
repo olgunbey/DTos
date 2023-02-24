cbab9232f6aa17760f37cef3dfda1c4d1356302a
olgunbey şahin <olgunsahin0161@hotmail.com>
2023-02-24 03:14:11 +0300

    DTos

---
+++ ConsoleApp1/Migrations/20230223235742_mg1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class mg1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "orders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orders", x => x.OrderId);
+                    table.ForeignKey(
+                        name: "FK_orders_Products_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_orders_ProductID",
+                table: "orders",
+                column: "ProductID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.EntityFrameworkCore;
+
+AppDbContext context = new();
+
+List<ProductOrderDTO> datas =await context.Products.Include(x => x.Orders).Select(p => new ProductOrderDTO()
+{
+    ProductId = p.ProductId,
+    ProductName=p.ProductName,
+    Orders=p.Orders
+}).ToListAsync();
+
+
+List<ProductOrderDTO2> datas2 = await context.Products.Include(x => x.Orders).Select(x => new ProductOrderDTO2() {
+    ProductName = x.ProductName,
+    Description= x.Orders.AsQueryable().Select(p=>p.Description).ToList()
+}).ToListAsync();
+
+
+Console.WriteLine("");
+
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string? ProductName { get; set; }
+    public ICollection<Order>? Orders { get; set; }
+}
+public class Order
+{
+    public int OrderId { get; set; }
+    public string? Description { get; set; }
+    public int ProductID { get; set; }
+    public Product? Product { get; set; }
+
+}
+public class ProductOrderDTO:Product
+{
+    public ICollection<Order>? Orders { get; set; }
+}
+public class ProductOrderDTO2
+{
+    public string? ProductName { get; set; }
+    public List<string>? Description { get; set; }
+}
+public class AppDbContext:DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> orders { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasKey(p=> p.ProductId);
+        modelBuilder.Entity<Product>().HasMany(p=>p.Orders).WithOne(o=>o.Product).HasForeignKey(f=>f.ProductID);
+        modelBuilder.Entity<Order>().HasKey(o=>o.OrderId);
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=OLGUNPC\\SQLEXPRESS; Initial Catalog=DTOS; Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+}
